ce838f5b8c78d337e23296f5e3b3ff4453b451a7
Shaun Khundker <shaun.khundker@gmail.com>
2025-04-18 14:52:44 -0400

    fix build

--- test/sTokenAuxiliary.t.sol
+++ test/sTokenAuxiliary.t.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {sTokenAuxiliary} from "src/sTokenAuxiliary.sol";
 import {ERC1967Proxy} from "openzeppelin/proxy/ERC1967/ERC1967Proxy.sol";
-import {ERC20} from "openzeppelin/token/ERC20.sol";
 
 contract sTokenAuxiliaryTest is Test {
     sTokenAuxiliary auxiliary;
@@ -15,14 +14,25 @@ contract sTokenAuxiliaryTest is Test {
 
     function setUp() public {
         owner = vm.addr(0xA11CE);
+        sToken = vm.addr(0x0001);
+        mintingContract = vm.addr(0x0002);
 
         auxiliary = sTokenAuxiliary(
             address(
                 new ERC1967Proxy(
                     address(new sTokenAuxiliary()),
-                    abi.encodeWithSignature("initialize(address,address,address,string,string)", sToken, owner, mintingContract, "wsTOKEN", "wsTOKEN")
+                    abi.encodeWithSignature(
+                        "initialize(address,address,address,string,string)",
+                        sToken,
+                        owner,
+                        mintingContract,
+                        "wsTOKEN",
+                        "wsTOKEN"
+                    )
                 )
             )
         );
     }
+
+    function testDefault() public {}
 }
